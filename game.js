cb2c78d4220cf0483608f674d21122a1ad6055db
RuslanMak <makarovemd@gmail.com>
2019-02-16 21:54:56 +0200

    doing game.js

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 'use strict';
+
+class Vector {
+  constructor() {
+
+  }
+}
+
+class Actor {
+  constructor() {
+
+  }
+}
+
+class Level {
+  constructor() {
+
+  }
+}
+
+
+const grid = [
+  new Array(3),
+  ['wall', 'wall', 'lava']
+];
+const level = new Level(grid);
+runLevel(level, DOMDisplay);
