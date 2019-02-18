eabf48af9122f589116fb8cbfe2a64468e1ed328
RuslanMak <makarovemd@gmail.com>
2019-02-18 16:45:57 +0200

    done vector

--- game.js
+++ game.js
@@ -1,9 +1,21 @@
 'use strict';
 
 class Vector {
-  constructor() {
-
+  constructor(x=0, y=0) {
+    this.x = x;
+    this.y = y;
   }
+
+  plus(vector) {
+		if (vector instanceof Vector) {
+			return new Vector(this.x + vector.x, this.y + vector.y);
+		}
+		throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+	}
+
+	times(n = 1) {
+		return new Vector(this.x * n, this.y * n);
+	}
 }
 
 class Actor {
@@ -25,3 +37,33 @@ const grid = [
 ];
 const level = new Level(grid);
 runLevel(level, DOMDisplay);
+
+const schemas = [
+  [
+    '         ',
+    '         ',
+    '    =    ',
+    '       o ',
+    '     !xxx',
+    ' @       ',
+    'xxx!     ',
+    '         '
+  ],
+  [
+    '      v  ',
+    '    v    ',
+    '  v      ',
+    '        o',
+    '        x',
+    '@   x    ',
+    'x        ',
+    '         '
+  ]
+];
+const actorDict = {
+  '@': Player,
+  'v': FireRain
+}
+const parser = new LevelParser(actorDict);
+runGame(schemas, parser, DOMDisplay)
+  .then(() => console.log('Вы выиграли приз!'));
