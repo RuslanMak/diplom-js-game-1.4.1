adaf7e230ca4c46929d601b35249674e3ceff290
RuslanMak <makarovemd@gmail.com>
2019-03-14 20:02:27 +0200

    I have made shorter "class Player"

--- game.js
+++ game.js
@@ -279,10 +279,7 @@ class Coin extends Actor {
 
 class Player extends Actor {
     constructor(pos) {
-        super(pos);
-        this.pos = this.pos.plus(new Vector(0, -0.5));
-        this.size = new Vector(0.8, 1.5);
-        this.speed = new Vector(0, 0);
+        super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5), new Vector(0, 0));
     }
 
     get type() {
